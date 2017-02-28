f862a6330eb03f7019d179e448e31d8c06bb6016
Udit <codenirvana28@gmail.com>
2017-03-01 00:39:39 +0530

    weather req

--- actions/weather.js
+++ actions/weather.js
@@ -1,7 +1,29 @@
 "use strict";
 
-const init = (params) => {
-  return params;
-}
+const axios = require('axios');
+const util = require('util');
+const {async, await} = require('asyncawait');
+const {keys} = require('../config');
+const token = keys.actions.weather;
+
+const URL = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=" + token;
+
+const fetch = city => {
+  return axios.get( util.format(URL, city) )
+    .then( response => {
+      const Weather = response.data;
+      const temp = Weather.main.temp;
+      const condition = Weather.weather[0].main;
+      return { error: false, data: `Its ${temp} °C in ${city} and looks ${condition}` };
+    })
+    .catch( error => {
+      return { error: true, message: "Internal Server Error" };
+    });
+};
+
+const init = async(params => {
+  let DATA = await( fetch(params['geo-city']) );
+  return DATA;
+});
 
 exports = module.exports = init;
--- app.js
+++ app.js
@@ -2,30 +2,35 @@
 
 const express = require('express');
 const apiai = require("apiai");
-const app = express();
+const {async, await} = require('asyncawait');
 const bodyParser = require('body-parser');
 const {keys} = require('./config');
 const actions = require('./actions');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
-
 const AI = apiai(keys.apiai.client);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
-    const request = AI.textRequest(req.query.q, { sessionId: '1234' });
+app.get('/', (req, res) => {
+    const request = AI.textRequest(req.query.q, { sessionId: req.query.s });
 
-    request.on('response', function(response) {
-        console.log(response);
+    request.on('response', async(response => {
+        console.log(JSON.stringify(response, undefined, 2));
+
+        const fulfillment = response.result.fulfillment.speech;
+        if(fulfillment) return res.send(fulfillment);
+
         const action = response.result.metadata.intentName;
         const params = response.result.parameters;
-        const data = actions[action](params);
-        res.send(data);
-    });
 
-    request.on('error', function(error) {
+        const data = await(actions[action](params));
+        res.json(data);
+    }));
+
+    request.on('error', error => {
         console.log(error);
         res.status(500).send('ERROR!');
     });
@@ -33,6 +38,6 @@ app.get('/', function(req, res){
     request.end();
 });
 
-const server = app.listen(PORT, function() {
+const server = app.listen(PORT, () => {
   console.log(`Listening @ http://localhost:${PORT}`);
 });
