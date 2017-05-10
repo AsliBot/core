260684e6b465f335c51a9bc03d47599b4ddcbdab
Udit <codenirvana28@gmail.com>
2017-05-10 22:07:20 +0530

    quotes action

---
+++ actions/quote.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const request = require('request');
+const {async, await} = require('asyncawait');
+
+const URL = "http://api.forismatic.com/api/1.0/";
+const POST_DATA = { method: 'getQuote', lang: 'en', format: 'json' };
+
+function fetch () {
+  const options = {
+    method: 'POST',
+    url: URL,
+    form: POST_DATA
+  };
+  return new Promise(function (resolve, reject) {
+    request(options, (error, res, body) => {
+      if (!error && res.statusCode == 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+const init = async((user, params) => {
+  let DATA = await( fetch() );
+  if (DATA) {
+    return { error: false, data: `${DATA.quoteText} \n - ${DATA.quoteAuthor}` };
+  }
+  return { error: false, data: "Error fetching quote" };
+});
+
+module.exports = init;
